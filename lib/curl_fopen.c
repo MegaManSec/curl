a0958f997276782f5d8701c03ba52277aadb7972
Joshua Rogers <MegaManSec@users.noreply.github.com>
2026-09-15 19:49:42 +0200

    curl_fopen: do not truncate an existing destination before the safe save
    Curl_fopen() used to open the destination filename directly with
    fopen(..., "w") to fstat() it and clone its mode onto the temp file it
    was about to create. That open followed a final-component symlink and
    truncated whatever it pointed to, before the O_CREAT|O_EXCL temp file
    and rename dance that is meant to protect the save ever got a chance
    to run. This affects CURLOPT_COOKIEJAR, CURLOPT_HSTS and
    CURLOPT_ALTSVC saves.
    
    Use a non-destructive stat() to inspect the destination and capture
    its mode instead, without ever opening it for writing. A regular file
    (or a symlink to one) is now left untouched until the temp file is
    renamed over it. A non-regular destination is still opened and written
    to directly, matching prior behavior.
    
    Reported-by: Joshua Rogers

--- lib/curl_fopen.c
+++ lib/curl_fopen.c
@@ -89,19 +89,31 @@ CURLcode Curl_fopen(struct Curl_easy *data, const char *filename,
   char *tempstore = NULL;
 #ifndef _WIN32
   curlx_struct_stat sb;
+#ifdef HAVE_FCHMOD
+  bool have_sb = FALSE;
+#endif
 #endif
   int fd = -1;
   char *dir = NULL;
   *tempname = NULL;
 
 #ifndef _WIN32
-  *fh = curlx_fopen(filename, FOPEN_WRITETEXT);
-  if(!*fh)
-    goto fail;
-  if(curlx_fstat(fileno(*fh), &sb) == -1 || !S_ISREG(sb.st_mode)) {
+  if(curlx_stat(filename, &sb) == -1) {
+    if(errno != ENOENT)
+      goto fail;
+  }
+  else if(!S_ISREG(sb.st_mode)) {
+    /* not a regular file, open and use it directly without the
+       temp file dance */
+    *fh = curlx_fopen(filename, FOPEN_WRITETEXT);
+    if(!*fh)
+      goto fail;
     return CURLE_OK;
   }
-  curlx_fclose(*fh);
+#ifdef HAVE_FCHMOD
+  else
+    have_sb = TRUE;
+#endif
 #endif /* !_WIN32 */
   *fh = NULL;
 
@@ -138,7 +150,7 @@ CURLcode Curl_fopen(struct Curl_easy *data, const char *filename,
     goto fail;
 
 #ifdef HAVE_FCHMOD
-  {
+  if(have_sb) {
     curlx_struct_stat nsb;
     if((curlx_fstat(fd, &nsb) != -1) &&
        (nsb.st_uid == sb.st_uid) && (nsb.st_gid == sb.st_gid)) {
